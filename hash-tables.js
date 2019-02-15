246288d817dd776549b6ee26169673b44ff63d64
tadej <tadej@mail.com>
2019-02-15 20:40:17 +0100

    hash table data structure

--- hash-tables.js
+++ hash-tables.js
@@ -1,44 +1,43 @@
-function HashTable() {
-this.table = new Array(137);
-this.simpleHash = simpleHash;
-this.betterHash = betterHash;
-this.showDistro = showDistro;
-this.put = put;
-//this.get = get;
-}
+/*
+* hash tables
+* for fast insertion, deletion, and retrieval
+* ach data element is stored in the array based on the key, similar to dictionary
+* to store a piece of data in a hash table, the key is mapped into a number in the range of 0 through the hash table size, using a hash function
+* not suitable for searching operations such as find max or min
+*/
+class HashTable{
+	
+	constructor () {
+		this.table = new Array(137);  // data storage - array with predefined length
+	}
+	
+	// put data to table, generate key first with hash function
+	put (data) {
+		const key = this.hash(data);
+		this.table[key] = data;
+	}
+	
+	// hash function
+	hash (string) {
+		const H = 37;
+		let total = 0;
+		for (let i = 0; i < string.length; i++) {
+			total += H * total + string.charCodeAt(i);
+		}
+		total = total % this.table.length;
+		if (total < 0) {
+			total += this.table.length - 1;
+		}
+		return parseInt(total);
+	}
+	
+	// display data - to console
+	display () {
+		for (let i = 0; i < this.table.length; i++) {
+			if (this.table[i] != undefined) {
+			console.log(i + ": " + this.table[i]);
+			}
+		}
+	}
 
-function put(data) {
-var pos = this.betterHash(data);
-this.table[pos] = data;
-}
-
-function simpleHash(data) {
-var total = 0;
-for (var i = 0; i < data.length; ++i) {
-total += data.charCodeAt(i);
-}
-console.log("Hash value: " + data + " -> " + total);
-return total % this.table.length;
-}
-
-function showDistro() {
-var n = 0;
-for (var i = 0; i < this.table.length; ++i) {
-if (this.table[i] != undefined) {
-console.log(i + ": " + this.table[i]);
-}
-}
-}
-
-function betterHash(string) {
-const H = 37;
-var total = 0;
-for (var i = 0; i < string.length; ++i) {
-total += H * total + string.charCodeAt(i);
-}
-total = total % this.table.length;
-if (total < 0) {
-total += this.table.length-1;
-}
-return parseInt(total);
 }
