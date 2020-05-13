08bb204e1773c717acfb30ed07c9c4ea34bede89
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-05-13 13:33:11 +0200

    lists

--- lists.js
+++ lists.js
@@ -1,24 +1,39 @@
-/*
-* a list is a ordered sequence of data
-* each data item stored in the list is called an element
-* elements can be of any data type
-*/
+/**
+ * Class List
+ * 
+ * A list is a ordered sequence of data
+ * Each data item stored in the list is called an element
+ * Elements can be of any data type
+ */
 class List {
 	
 	constructor() {
-		this.listSize = 0; 		// size of the list
-		this.pos = 0; 			// current position in the list
-		this.data = []; 		// data storage - elements stored in an array
+		this.listSize = 0; 		// Size of the list
+		this.pos = 0; 			// Current position in the list
+		this.data = []; 		// Data storage - elements stored in an array
 	}
 	
-	// append element to list
+	
+	/**
+	 * Append element to list
+	 * 
+	 * @param {string} el 
+	 */
 	append(el) {
 		this.data[this.listSize++] = el;
 	}
 	
-	// insert an element
+
+	/**
+	 * Insert an element at specific position
+	 * 
+	 * @param {string} el 
+	 * @param {string} after 
+	 * 
+	 * @returns {boolean}
+	 */
 	insert(el, after) {
-		var insertPos = this.find(after);
+		const insertPos = this.find(after);
 		if (insertPos > -1) {
 			this.data.splice(insertPos + 1, 0, el);
 			this.listSize++;
@@ -27,9 +42,16 @@ class List {
 		return false;
 	}
 	
-	// remove element from list
+	
+	/**
+	 * Remove element from the list
+	 * 
+	 * @param {string} el 
+	 * 
+	 * @returns {boolean}
+	 */
 	remove(el) {
-		var foundAt = this.find(el);
+		const foundAt = this.find(el);
 		if (foundAt > -1) {
 			this.data.splice(foundAt, 1);
 			this.listSize--;
@@ -38,62 +60,125 @@ class List {
 		return false;
 	}
 	
-	// empty list
+	
+	/**
+	 * Empty the list
+	 */
 	clear() {
 		this.data = [];
 		this.listSize = 0;
 		this.pos = 0;
 	}
 	
-	// find element in the list
+
+	/**
+	 * Find element in the list
+	 * 
+	 * @param {string} el
+	 * 
+	 * @returns {number} index
+	 */
 	find(el) {
 		return this.data.indexOf(el);
 	}
 	
-	// diplay list
+
+	/**
+	 * Display the list to console
+	 * 
+	 */
 	display() {
-		return this.data;
+		console.log(this.data);
 	}
 	
-	// check if element is in the list
+
+	/**
+	 * Check if element is in the list
+	 * 
+	 * @param {string} el 
+	 * 
+	 * @returns {boolean}
+	 */
 	hasElement(el) {
 		return this.data.includes(el);
 	}
 	
-	// traverse the list methods
+
+	/**
+	 * Move to front of the list
+	 * 
+	 */
 	front() {
 		this.pos = 0;
 	}
 	
+
+	/**
+	 * Move to end of the list
+	 * 
+	 */
 	end() {
 		this.pos = this.listSize - 1;
 	}
 	
+
+	/**
+	 * Move to previous element
+	 * 
+	 */
 	prev() {
 		if (this.pos > 0) {
 			this.pos--;
 		}
 	}
 	
+
+	/**
+	 * Move to next element
+	 * 
+	 */
 	next() {
 		if (this.pos < this.listSize - 1) {
 			this.pos++;
 		}
 	}
 	
+
+	/**
+	 * Get position of the current element in the list
+	 * 
+	 * @returns {number} index
+	 */
 	currPos() {
 		return this.pos;
 	}
 	
+
+	/**
+	 * Move to position
+	 * 
+	 * @param {number} position 
+	 */
 	moveTo(position) {
 		this.pos = position;
 	}
 	
+
+	/**
+	 * Get current element value
+	 * 
+	 * @returns {string} el
+	 */
 	getElement() {
 		return this.data[this.pos];
 	}
 	
-	// return length of the list
+	
+	/**
+	 * Get size of the list
+	 * 
+	 * @returns {number} length
+	 */
 	length() {
 		return this.listSize;
 	}
