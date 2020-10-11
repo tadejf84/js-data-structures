07d52a8dc6e29e6a2b48e7280cfa6f7ac1c7eaf5
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-11 19:04:34 +0200

    lists

--- queue.js
+++ queue.js
@@ -4,71 +4,71 @@
  * First in, first out data structure
  */
 class Queue {
+    
+    /**
+     * @constructor
+     * 
+     */
+    constructor() {
+        this.data = [];   // data storage
+    }
 	
-	constructor() {
-		this.data = [];   // data storage
-	}
+    /**
+     * Add element to back of the queue
+     * 
+     * @param {string} el 
+     */
+    enqueue(el) {
+        this.data.push(el);
+    }
+
+    /**
+     * Remove element from front
+     * 
+     */
+    dequeue() {
+        this.data.shift();
+    }
+
+    /**
+     * Traverse to front
+     * 
+     * @returns {string} element from front
+     */
+    front() {
+        return this.data[0];
+    }
+
+    /**
+     * Traverse to back
+     * 
+     * @returns {string} last element
+     */
+    back() {
+        return this.data[this.data.length - 1];
+    }
+
+    /**
+     * Display queue in console
+     * 
+     */
+    display() {
+        for (let d of this.data) {
+            console.log(d + ',');
+        }
+    }
 	
+    /**
+     * Check if queue is empty
+     * 
+     * @returns {boolean}
+     */
+    isEmpty() {
+        if (this.data.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	/**
-	 * Add element to back of the queue
-	 * 
-	 * @param {string} el 
-	 */
-	enqueue(el) {
-		this.data.push(el);
-	}
-
-
-	/**
-	 * Remove element from front
-	 * 
-	 */
-	dequeue() {
-		this.data.shift();
-	}
-
-
-	/**
-	 * Traverse to front
-	 * 
-	 */
-	front() {
-		return this.data[0];
-	}
-
-
-	/**
-	 * Traverse to back
-	 * 
-	 */
-	back() {
-		return this.data[this.data.length - 1];
-	}
-	
-
-	/**
-	 * Display queue in console
-	 * 
-	 */
-	display() {
-		for (let d of this.data) {
-			console.log(d + ',');
-		}
-	}
-	
-
-	/**
-	 * Check if queue is empty
-	 * 
-	 * @returns {boolean}
-	 */
-	isEmpty() {
-		if (this.data.length === 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
 }
